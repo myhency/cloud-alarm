5a8c8483ffdd3e00f0e706a8e9d3f80a13aaf0cd
myhency <hency.yeo@gmail.com>
2021-05-16 21:17:37 +0900

    CLOUD-41 bug fix

--- src/services/stockItems.js
+++ src/services/stockItems.js
@@ -1,6 +1,7 @@
 import { stockItems } from '../../fixture/stockItems';
 
-export default async function fetchStockItemList() {
+// eslint-disable-next-line import/prefer-default-export
+export async function fetchStockItemList() {
   return new Promise((resolve, _) => setTimeout(() => resolve({
     result: true,
     data: stockItems,
