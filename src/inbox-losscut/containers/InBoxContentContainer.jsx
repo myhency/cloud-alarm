62b5c3230a642547a6bfe1102629a2c4461de62b
myhency <hency.yeo@gmail.com>
2021-06-24 22:37:02 +0900

    design refactoring

--- src/inbox-losscut/containers/InBoxContentContainer.jsx
+++ src/inbox-losscut/containers/InBoxContentContainer.jsx
@@ -19,11 +19,13 @@ import {
 // Icons
 import DeleteIcon from '@material-ui/icons/Delete';
 import GetAppIcon from '@material-ui/icons/GetApp';
-import EditIcon from '@material-ui/icons/Edit';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import RedoIcon from '@material-ui/icons/Redo';
 import RefreshIcon from '@material-ui/icons/Refresh';
+import { useStyles } from '../../common/components/Styles';
+import { SearchInput } from '../../common/components/Inputs';
+import { StyledTooltip } from '../../common/components/Tooltips';
 
 import {
   loadLosscutAlarmList,
@@ -32,100 +34,6 @@ import {
 } from '../../state/slice';
 
 import InBoxModalContainer from './InBoxModalContainer';
-
-const useStyles = makeStyles((theme) => ({
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    // padding: theme.spacing(3),
-  },
-  tableHeaderRoot: {
-    display: 'flex',
-    alignItems: 'center',
-    borderBottom: '1px solid lightgrey',
-    height: '60px',
-    paddingTop: '5px',
-    paddingBottom: '5px',
-  },
-  tableRoot: {
-    width: '100%',
-  },
-  paper: {
-    width: '100%',
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1,
-  },
-  checkbox: {
-    width: '48px',
-    padding: '0 0 0 0',
-  },
-  typographySub: {
-    color: 'grey',
-  },
-}));
-
-const StyledTooltip = withStyles({
-  tooltip: {
-    backgroundColor: 'rgba(0,0,0,0.72)',
-    fontSize: 12,
-    marginTop: 0,
-  },
-})(Tooltip);
-
-const SearchInput = withStyles((theme) => ({
-  root: {
-    'label + &': {
-      marginTop: theme.spacing(3),
-    },
-  },
-  input: {
-    borderRadius: 4,
-    backgroundColor: '#ced4da',
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    padding: '10px 12px',
-    minWidth: '300px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-      borderColor: '#ced4da',
-      backgroundColor: '#ecfeff',
-    },
-  },
-}))(InputBase);
 
 export default function InBoxContentContainer() {
   const history = useHistory();
