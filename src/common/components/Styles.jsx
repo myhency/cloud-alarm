3b4200f9d92d3afe547b2cd4e871522cb38cd932
myhency <hency.yeo@gmail.com>
2021-06-25 00:05:55 +0900

    Comming soon 추가

--- src/common/components/Styles.jsx
+++ src/common/components/Styles.jsx
@@ -7,14 +7,11 @@ export const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'flex-end',
     padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
     ...theme.mixins.toolbar,
   },
   content: {
     flexGrow: 1,
-    // padding: theme.spacing(3),
     height: '100%',
-    // backgroundColor: '#FBE8A6',
   },
   tableHeaderRoot: {
     display: 'flex',
